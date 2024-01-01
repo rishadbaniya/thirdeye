cc821847c1e57188e1ec25d2d45dd878c8d54323
rishadbaniya <rbdroid01@gmail.com>
2024-01-01 07:03:08 +0545

    feat: add live preview of data through polling and description for device

--- third-eye-admin/src/devices/DeviceEdit.tsx
+++ third-eye-admin/src/devices/DeviceEdit.tsx
@@ -10,12 +10,16 @@ import {
   Edit,
   useRecordContext,
   useInput,
+  TextField,
 } from "react-admin";
 import { SelectInput } from "react-admin";
 import { Box } from "@mui/material";
 import { ImageInput, ImageField } from "react-admin";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import axios from "axios";
+import { format, differenceInSeconds } from "date-fns";
 
 const DeviceEdit = (props) => {
   return (
@@ -24,7 +28,32 @@ const DeviceEdit = (props) => {
     </Edit>
   );
 };
+
+const API_URl = "http://localhost:8000/devices";
 const DeviceEditForm = () => {
+  const id = useRecordContext().id;
+  const [compResource, updateCompResource] = useState({});
+
+  useEffect(() => {
+    // Function to fetch data using Axios
+    const fetchData = async () => {
+      axios
+        .get(`${API_URl}/${id}`)
+        .then((response) => {
+          console.log(response.data);
+          updateCompResource({ compResource, ...response.data });
+        })
+        .catch((error) => {
+          // Handle errors
+          console.error("Error fetching data:", error.message);
+        });
+    };
+
+    const pollingInterval = 2000;
+    const pollingId = setInterval(fetchData, pollingInterval);
+    return () => clearInterval(pollingId);
+  }, [id]);
+
   const validateDeviceEdit = (values: DeviceCreateValues) => {
     const errors: DeviceCreateValues = {};
     if (!values.device_id) {
@@ -78,9 +107,78 @@ const DeviceEditForm = () => {
               />
             </Box>
           </Box>
+
+          <Box flex={1} display={"flex"} flexDirection="row">
+            {compResource.resource ? (
+              <ResourceBox input={compResource} />
+            ) : (
+              <></>
+            )}
+          </Box>
         </Box>
       </SimpleForm>
     </Box>
+  );
+};
+
+const ResourceBox = (props) => {
+  let isActive = (epochTime: any) => {
+    const date = new Date(epochTime * 1000);
+    const formattedDate = format(date, "yyyy-MM-dd HH:mm:ss");
+
+    const currentDateTime = new Date();
+    const difference = differenceInSeconds(currentDateTime, date);
+
+    return difference <= 30;
+  };
+  console.log(props);
+  return (
+    <>
+      {props.input.resource[0] ? (
+        <Box display="flex" flexDirection="column" width="100%">
+          <Box flex={1} ml={{ xs: 0, sm: "0.5em" }} mr={{ xs: 0, sm: "0.5em" }}>
+            <Box flex={1} display={"flex"} flexDirection="row">
+              <p>
+                Is Online :{" "}
+                <b>{isActive(props.input.resource[0].time) ? "Yes" : "No"}</b>
+              </p>
+            </Box>
+            <Box flex={1} display={"flex"} flexDirection="row">
+              <p>CPU Cores : {props.input.resource[0].cpu_cores}</p>
+            </Box>
+            <Box flex={1} display={"flex"} flexDirection="row">
+              <p>CPU Brand : {props.input.resource[0].cpu_brand}</p>
+            </Box>
+            <Box flex={1} display={"flex"} flexDirection="row">
+              <p>CPU Frequency : {props.input.resource[0].cpu_frequency}</p>
+            </Box>
+            <Box flex={1} display={"flex"} flexDirection="row">
+              <p>
+                Memory Size :{" "}
+                {props.input.resource[0].memory_size / 1024 / 1024} <b>Bytes</b>
+              </p>
+            </Box>
+            <Box flex={1} display={"flex"} flexDirection="row">
+              <p>
+                Memory Size Available:{" "}
+                {props.input.resource[0].memory_available} <b>Bytes</b>
+              </p>
+            </Box>
+            <Box flex={1} display={"flex"} flexDirection="row">
+              <p>
+                Memory Size Used: {props.input.resource[0].memory_used}{" "}
+                <b>Bytes</b>
+              </p>
+            </Box>
+            <Box flex={1} display={"flex"} flexDirection="row">
+              <p>Uptime: {props.input.resource[0].uptime} seconds</p>
+            </Box>
+          </Box>
+        </Box>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
--- third-eye-admin/src/map.jsx
+++ third-eye-admin/src/map.jsx
@@ -5,93 +5,119 @@ import { useRedirect } from "react-admin";
 
 import { GeoJSON, MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
+import axios from "axios";
 
 const geojsonPath = "/assets/geoJSON-Nepal/nepal-districts-new.geojson";
 
 const mapStyle = {
-  width: "100%",
-  height: "100%",
-  margin: "10px auto"
-  // padding: "10px"
+    width: "100%",
+    height: "100%",
+    margin: "10px auto",
+    // padding: "10px"
 };
 
 const countryBounds = [
-  [26.347, 80.058], // Southwestern corner
-  [30.447, 88.201] // Northeastern corner
+    [26.347, 80.058], // Southwestern corner
+    [30.447, 88.201], // Northeastern corner
 ];
 
 let markerIcon = new L.Icon({
-  iconUrl: "https://cdn-icons-png.flaticon.com/512/13484/13484237.png", // normal emoji icon
-  iconSize: [40, 40],
-  iconAnchor: [16, 32]
+    iconUrl: "https://cdn-icons-png.flaticon.com/512/13484/13484237.png", // normal emoji icon
+    iconSize: [40, 40],
+    iconAnchor: [16, 32],
 });
 
 const center = [28.3949, 84.124]; // Kathmandu
 
 const MapButton = ({ id }) => {
-  const redirect = useRedirect();
-  const handleClick = () => {
-    const finalpath = `/groups/${id}`;
-    redirect(finalpath);
-  };
-  return <button onClick={handleClick}>Dashboard</button>;
+    const redirect = useRedirect();
+    const handleClick = () => {
+        const finalpath = `/devices/${id}`;
+        redirect(finalpath);
+    };
+    return <button onClick={handleClick}>Dashboard</button>;
 };
 
 const GroupIcon = ({ lat, lon, id }) => {
-  return (
-    <Marker key={1} position={[lat, lon]} icon={markerIcon}>
-      <Popup>
-        <MapButton id={id}>Visit Group!</MapButton>
-      </Popup>
-    </Marker>
-  );
+    return (
+        <Marker key={1} position={[lat, lon]} icon={markerIcon}>
+            <Popup>
+                <MapButton id={id}>Visit Group!</MapButton>
+            </Popup>
+        </Marker>
+    );
 };
-
 const NepalMap = () => {
-  const [geoJSON, setGeoJSON] = React.useState(null);
-  // const redirect = useRedirect();
-  // const handleClick = () => {
-  //   // redirect(`/groups/${group_id}`);
-  //   redirect(`/groups`);
-  // };
+    const [geoJSON, setGeoJSON] = React.useState(null);
+    const [devices, updateDevices] = React.useState([]);
 
-  useEffect(() => {
-    const fetchGeoJSON = async () => {
-      const response = await fetch(geojsonPath);
-      const json = await response.json();
-      setGeoJSON(json);
-    };
-    fetchGeoJSON();
-  }, []);
+    // const handleClick = () => {
+    //   // redirect(`/groups/${group_id}`);
+    //   redirect(`/groups`);
+    // };
 
-  return (
-    <MapContainer
-      style={mapStyle}
-      center={center}
-      zoom={8}
-      minZoom={8}
-      maxBounds={countryBounds}
-      maxBoundsViscosity={1.0}
-    >
-      {geoJSON && (
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      )}
-      {geoJSON && (
-        <GeoJSON
-          data={geoJSON}
-          style={() => {
-            return {
-              color: "black",
-              weight: 1,
-              fillColor: "grey",
-              fillOpacity: 0.5
-            };
-          }}
-        />
-      )}
-      <GroupIcon lat={center[0]} lon={center[1]} id={1} />
-    </MapContainer>
-  );
+    useEffect(() => {
+        console.log("I CAMMMMMMMMMMMMMMMMMMMMMMMMMMEEEEEEEEEEEEEEEe");
+
+        const fetchData = async () => {
+            axios
+                .get(`http://localhost:8000/devices`)
+                .then((response) => {
+                    console.log(response);
+                    updateDevices(response.data.data);
+                })
+                .catch((error) => {
+                    console.error("Error fetching data:", error.message);
+                });
+        };
+
+        fetchData();
+
+        const fetchGeoJSON = async () => {
+            const response = await fetch(geojsonPath);
+            const json = await response.json();
+            setGeoJSON(json);
+        };
+        fetchGeoJSON();
+    }, []);
+
+    return (
+        <MapContainer
+            style={mapStyle}
+            center={center}
+            zoom={8}
+            minZoom={8}
+            maxBounds={countryBounds}
+            maxBoundsViscosity={1.0}
+        >
+            {geoJSON && (
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            )}
+            {geoJSON && (
+                <GeoJSON
+                    data={geoJSON}
+                    style={() => {
+                        return {
+                            color: "black",
+                            weight: 1,
+                            fillColor: "grey",
+                            fillOpacity: 0.5,
+                        };
+                    }}
+                />
+            )}
+            {devices.map((device, i) => {
+                return (
+                    <GroupIcon
+                        key={i}
+                        lat={device.latitude}
+                        lon={device.longitude}
+                        id={device.id}
+                    />
+                );
+            })}
+        </MapContainer>
+    );
 };
 
 export default NepalMap;
